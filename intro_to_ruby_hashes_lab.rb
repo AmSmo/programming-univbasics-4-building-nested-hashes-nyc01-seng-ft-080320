58c4800162ed220713ed2c5f4843e270dde25ab6
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-19 13:56:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,12 +31,13 @@ def monopoly_with_third_tier
                   three_pieces_owned: 100,
                   four_pieces_owned: 200,
                 }
-                  },
-                names: {reading_railroad: {},
+                  ,
+                names: {
+                  reading_railroad: {},
                   
                 }
                }
-  
+  }
   
   
   
